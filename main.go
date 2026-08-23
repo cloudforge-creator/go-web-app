be18238c3c23bde21a4975484f693e22c16b021b
Muhammad Ahad <ahadch4471@gmail.com>
2026-08-23 23:38:30 +0500

    feat: add Go web server

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/home.html")
+}
+
+func coursePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/journey.html")
+}
+
+func aboutPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/about.html")
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/contact.html")
+}
+
+func main() {
+
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/home", homePage)
+	http.HandleFunc("/journey", coursePage)
+	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/contact", contactPage)
+
+	log.Println("Server running at http://localhost:8080")
+
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
